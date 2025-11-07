3cb1ff4dcfd874773a3f513460949f6ecad7aefd
ArnoSchiller <arno-schiller@hotmail.de>
2025-11-07 10:16:40 +0100

    Integrated database stored JsonFormsDefinition

--- SWMS.Components.Blazor.Module/JsonForms/JsonFormsProperyEditor.cs
+++ SWMS.Components.Blazor.Module/JsonForms/JsonFormsProperyEditor.cs
@@ -1,35 +1,54 @@
-﻿using DevExpress.ExpressApp.Blazor.Editors;
+﻿using DevExpress.Data.Filtering;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Blazor.Editors;
 using DevExpress.ExpressApp.Blazor.Editors.Adapters;
+using DevExpress.ExpressApp.Editors;
 using DevExpress.ExpressApp.Model;
+using SWMS.Components.Blazor.Module.JsonForms.BusinessObjects;
 
 namespace SWMS.Components.Blazor.Module.JsonForms;
 
-public class JsonFormsProperyEditor : BlazorPropertyEditorBase
+public class JsonFormsProperyEditor : BlazorPropertyEditorBase, IComplexViewItem
 {
-    private JsonFormsViewModel _componentModel;
+    public IObjectSpace ObjectSpace { get; set; }
+    public XafApplication Application { get; set; }
 
+    private string relatedProperty = "";
     public JsonFormsProperyEditor(Type objectType, IModelMemberViewItem model) : base(objectType, model) 
-    { 
-        _componentModel = new JsonFormsViewModel();
-        _componentModel.JsonSchema = @"{
-    ""type"": ""object"",
-    ""properties"": {
-        ""firstName"": { ""type"": ""string"" },
-        ""lastName"": { ""type"": ""string"" },
-        ""age"": { ""type"": ""integer"" },
-        ""isEmployed"": { ""type"": ""boolean"" },
-        ""birthDate"": { ""type"": ""string"", ""format"": ""date"" },
-        ""address"": {
-        ""type"": ""object"",
-        ""properties"": {
-            ""street"": { ""type"": ""string"" },
-            ""city"": { ""type"": ""string"" },
-            ""postalCode"": { ""type"": ""string"" }
-        }
-        }
-    }
-}";
+    {
+        relatedProperty = $"{objectType.FullName}.{model.PropertyName}";
     }
 
-    protected override IComponentAdapter CreateComponentAdapter() => new JsonFormsAdapter(_componentModel);
+    public void Setup(IObjectSpace objectSpace, XafApplication application)
+    {
+        ObjectSpace = objectSpace;
+        Application = application;
+    }
+
+    protected override IComponentAdapter CreateComponentAdapter() 
+    {
+        var jsonFormsDefinition = ObjectSpace.GetObjects<JsonFormsDefinition>(
+            CriteriaOperator.Parse("RelatedProperty = ?", relatedProperty)).FirstOrDefault();
+
+        if(jsonFormsDefinition == null)
+        {
+            jsonFormsDefinition = ObjectSpace.CreateObject<JsonFormsDefinition>();
+            jsonFormsDefinition.RelatedProperty = relatedProperty;
+            jsonFormsDefinition.JsonSchema = "";
+            jsonFormsDefinition.JsonFormsUiSchema = "";
+            ObjectSpace.CommitChanges();
+        }
+
+        if (string.IsNullOrEmpty(jsonFormsDefinition.JsonFormsUiSchema))
+        {
+            jsonFormsDefinition.JsonFormsUiSchema = JsonFormsHelper.GenerateUiSchema(jsonFormsDefinition.JsonSchema).ToJsonString();
+            ObjectSpace.CommitChanges();
+        }
+
+        var componentModel = new JsonFormsViewModel();
+        componentModel.JsonSchema = jsonFormsDefinition?.JsonSchema ?? "";
+        componentModel.UiSchema = jsonFormsDefinition?.JsonFormsUiSchema ?? "";
+
+        return new JsonFormsAdapter(componentModel);
+    }
 }
