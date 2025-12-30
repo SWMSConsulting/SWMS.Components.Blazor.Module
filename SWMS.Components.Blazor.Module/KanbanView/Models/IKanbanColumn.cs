bf434f2267a1cdb90351cc252d45eb29c20bb48c
ArnoSchiller <arno-schiller@hotmail.de>
2025-12-30 13:41:34 +0100

    Defined interfaces

---
+++ SWMS.Components.Blazor.Module/KanbanView/Models/IKanbanColumn.cs
@@ -0,0 +1,7 @@
+﻿namespace SWMS.Components.Blazor.Module.KanbanView.Models;
+
+public interface IKanbanColumn
+{
+    public string KanbanColumnName { get; }
+    public int KanbanColumnIndex { get; }
+}
---
+++ SWMS.Components.Blazor.Module/KanbanView/Models/IKanbanItem.cs
@@ -0,0 +1,8 @@
+﻿namespace SWMS.Components.Blazor.Module.KanbanView.Models;
+
+public interface IKanbanItem
+{
+    public string KanbanTitle { get; }
+    public string KanbanDescription { get; }
+    public IKanbanColumn KanbanColumn { get; }
+}
