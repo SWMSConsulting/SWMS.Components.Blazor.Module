fa13ac5771e61ace72049fe244df0652095a84e7
ArnoSchiller <arno-schiller@hotmail.de>
2026-01-05 10:00:22 +0100

    Included detailed control on drag and drop behaviour

--- SWMS.Components.Blazor.Module/KanbanView/Models/IKanbanItem.cs
+++ SWMS.Components.Blazor.Module/KanbanView/Models/IKanbanItem.cs
@@ -5,4 +5,8 @@ public interface IKanbanItem
     public string KanbanTitle { get; }
     public string KanbanDescription { get; }
     public IKanbanColumn KanbanColumn { get; set; }
+
+    bool IsDragEnabled { get; }
+
+    bool CanDropTo(IKanbanColumn targetColumn);
 }
