49c037ff9d31718a4497e6f775053ec1f68086a9
ArnoSchiller <arno-schiller@hotmail.de>
2026-01-05 10:15:03 +0100

    Made CurrentItem nullable

--- SWMS.Components.Blazor.Module/KanbanView/KanbanDragKontext.cs
+++ SWMS.Components.Blazor.Module/KanbanView/KanbanDragKontext.cs
@@ -4,5 +4,5 @@ using SWMS.Components.Blazor.Module.KanbanView.Models;
 namespace SWMS.Components.Blazor.Module.KanbanView;
 public static class KanbanDragContext
 {
-    public static IKanbanItem CurrentItem { get; set; }
+    public static IKanbanItem? CurrentItem { get; set; }
 }
